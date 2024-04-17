73386bcfe111764c2788aeae14c3fbafe64af104
Aurnab <zubayeraurnab@gmail.com>
2024-04-17 16:01:56 +0600

    first commit

---
+++ M-1/output/script.js
@@ -0,0 +1,6 @@
+"use strict";
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(11, 5));
+//array 
---
+++ M-1/src/script.ts
@@ -0,0 +1,7 @@
+function multiply(a: number, b: number) {
+  return a * b;
+}
+console.log(multiply(11, 5));
+
+//array 
+
